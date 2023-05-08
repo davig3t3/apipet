d2062fc527af3326b385e63fa588295e95485749
davig3t3 <davig3t3@gmail.com>
2023-05-07 23:29:07 -0500

    Initial commit

---
+++ ApipetApi/src/main/java/co/edu/uco/apipet/api/init/ApipetApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.apipet.api.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipetApiApplication.class, args);
+	}
+
+}
---
+++ ApipetApi/src/test/java/co/edu/uco/apipet/api/init/ApipetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.apipet.api.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
